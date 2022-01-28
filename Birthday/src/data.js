23c69b7672ed9f63dc5688328723da24cb26319f
Akshar Goyal <38995624+AksharGoyal@users.noreply.github.com>
2022-01-28 16:10:05 +0530

    Add files via upload

---
+++ Birthday/src/data.js
@@ -0,0 +1,45 @@
+const data = [
+    {
+      id: 1,
+      name: 'Bertie Yates',
+      age: 29,
+      image:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80',
+    },
+    {
+      id: 2,
+      name: 'Hester Hogan',
+      age: 32,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+      id: 3,
+      name: 'Larry Little',
+      age: 36,
+      image:
+        'https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo=',
+    },
+    {
+      id: 4,
+      name: 'Sean Walsh',
+      age: 34,
+      image:
+        'https://media.istockphoto.com/photos/headshot-portrait-of-smiling-ethnic-businessman-in-office-picture-id1300512215?b=1&k=20&m=1300512215&s=170667a&w=0&h=LsZL_-vvAHB2A2sNLHu9Vpoib_3aLLkRamveVW3AGeQ=',
+    },
+    {
+      id: 5,
+      name: 'Lola Gardner',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+    {
+        id: 6,
+        name: 'Akshar Goyal',
+        age: 25,
+        image:
+          'https://res.cloudinary.com/jerrick/image/upload/c_thumb,h_208,pg_1,q_40,w_208/h5f9xwwvvsf6cvf81wt9.jpg',
+      },
+  ];
+export default data;
